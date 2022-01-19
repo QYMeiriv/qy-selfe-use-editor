8be15552fd951826cde2f52857badbd534f452c4
QYMeiriv <2452063722@qq.com>
2022-01-19 13:47:46 +0800

    style: 💅 修改字体样式

--- src/styles/editor.ts
+++ src/styles/editor.ts
@@ -10,7 +10,7 @@ export const StyledEditor = styled("div")<{
   font-family: ${props => props.theme.fontFamily};
   letter-spacing: ${props => props.theme.letterSpacing};
   font-weight: ${props => props.theme.fontWeight};
-  font-size: 1em;
+  font-size: 0.96em;
   line-height: 1.74em;
   width: 100%;
 
